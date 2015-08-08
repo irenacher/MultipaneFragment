bdd7eef88b2d4be7b92a6d3c7588183c4df5e8a0
irenacher <irenac@restorationrobotics.com>
2015-08-07 17:09:31 -0700

    Initial commit

---
+++ app/src/main/java/com/example/irenachernyak/multipanefragment/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.irenachernyak.multipanefragment;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Configuration config = getResources().getConfiguration();
+
+        if(config.orientation == Configuration.ORIENTATION_PORTRAIT){
+            hideSidePanel();
+        }
+
+
+     }
+
+    private void hideSidePanel(){
+        View sidepane = findViewById(R.id.side_panel_fragment);
+        if(sidepane.getVisibility() == View.VISIBLE){
+            sidepane.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
